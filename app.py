d5c34d59717e24c30a63cce5836586a77a248a15
Kars <97431994+Karscoding@users.noreply.github.com>
2023-03-23 14:40:28 +0100

    Connectie met Flask
    Een aantal HTML bestanden zijn nu werkend via Flask, ook werken de CSS bestanden hierbij met {{url_for}}

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def root():
     return render_template('main.html')
 
+@app.route("/Lijst")
+def Lijst():
+    return render_template("Lijst.html")
+
 if __name__=='__main__':
     app.run(debug=True, port=3333)
